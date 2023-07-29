541b13dd6c8a5c8dce28560130ae230544b9be39
유재덕 <pationcook@naver.com>
2023-07-29 11:04:54 +0900

    Initial commit

---
+++ src/main/java/com/study/ducky/DuckyApplication.java
@@ -0,0 +1,13 @@
+package com.study.ducky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuckyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/ducky/DuckyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ducky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
